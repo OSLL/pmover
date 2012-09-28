340274178f9c5cb0d254a8f3877300c5dfc2780d
Aleksandr Kartashov <aleksandr.kartashov.aptu.se@gmail.com>
2012-09-28 19:44:53 +0400

    x86_64: architecture-specific bitops are moved from the file incldue/bitops.h to arch/x86_64/arch_bitops.h

--- include/bitops.h
+++ include/bitops.h
@@ -1,8 +1,6 @@
 #ifndef CR_BITOPS_H_
 #define CR_BITOPS_H_
 
-#ifdef CONFIG_X86_64
-
 #define DIV_ROUND_UP(n,d)	(((n) + (d) - 1) / (d))
 #define BITS_TO_LONGS(nr)	DIV_ROUND_UP(nr, 8 * sizeof(long))
 
@@ -19,35 +17,6 @@
 
 #define ADDR				BITOP_ADDR(addr)
 
-static inline void set_bit(int nr, volatile unsigned long *addr)
-{
-	asm volatile("bts %1,%0" : ADDR : "Ir" (nr) : "memory");
-}
-
-static inline void change_bit(int nr, volatile unsigned long *addr)
-{
-	asm volatile("btc %1,%0" : ADDR : "Ir" (nr));
-}
-
-static inline int test_bit(int nr, volatile const unsigned long *addr)
-{
-	int oldbit;
-
-	asm volatile("bt %2,%1\n\t"
-		     "sbb %0,%0"
-		     : "=r" (oldbit)
-		     : "m" (*(unsigned long *)addr), "Ir" (nr));
-
-	return oldbit;
-}
-
-static inline void clear_bit(int nr, volatile unsigned long *addr)
-{
-	asm volatile("btr %1,%0" : ADDR : "Ir" (nr));
-}
-
-#else /* CONFIG_X86_64 */
-# error x86-32 is not implemented yet
-#endif /* CONFIG_X86_64 */
+#include <arch_bitops.h>
 
 #endif /* CR_BITOPS_H_ */
